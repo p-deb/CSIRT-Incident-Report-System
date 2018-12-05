34ce2cd1fd416395c8eef197f9c8371db9148e29
p-deb <proggadeb@mail.adelphi.edu>
2018-12-05 16:07:06 -0500

    login page

---
+++ Login.php
@@ -0,0 +1,34 @@
+<?php
+
+	$host = 'localhost';
+	$user = 'proggadeb';
+	$pw = '';
+	$database = 'proggadeb';
+	
+	$db = new mysqli($host, $user, $pw, $database);
+	if ($db->connect_errno)
+	{
+		echo "Connect failed: ". $db->connect_error;
+		exit();
+	}
+
+	if (isset($_REQUEST['user']))
+	{	$user = $_REQUEST['user'];	}
+	if (isset($_REQUEST['pass']))
+	{	$pass = $_REQUEST['pass'];	}
+		
+	if (isset($_REQUEST['user']))
+	{
+#		$sqlLogin = "SELECT * FROM login WHERE username = '$user' AND password = '$pass'";
+		
+		if ("username = $user AND password = $pass")
+		{
+			include 'Homepage.php';
+		}
+		else
+		{
+			echo "Error: " . $sqlLogin . "<br>" . $db->error;
+		}
+	}
+	
+?>

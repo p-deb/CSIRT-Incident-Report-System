0e71ed659d2ff603d857bd8bcd0c768b54610c1e
p-deb <45210221+p-deb@users.noreply.github.com>
2018-12-05 21:50:02 -0500

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -1,40 +1,3 @@
-
-<!-- SCRIPT TO DISPLAY QUEUE IN DESCENDING ORDER (RECENT TO OLDEST TICKETS CREATED) -->
-<?php
-        echo "<link rel='stylesheet' type='text/css' href='Homepage.css' />";
-
-//        $host = 'localhost';
-  //      $user = 'proggadeb';
-//        $pw = '';
-  //      $database = 'proggadeb';
-//
-  //      $db = new mysqli($host, $user, $pw, $database);
-    //    if ($db->connect_errno)
-      //  {
-        //        echo "Connect failed: ". $db->connect_error;
-          //      exit();
-       // }
-
-$sql = "SELECT * FROM Incident ORDER BY dateCreated DESC";
-        $result = $db->query($sql);
-
-        $table = $result->fetch_all();
-        //var_dump($table);
-        echo "<table border = '1'>";
-//	echo "<tr><th>Incident #</th><th>Incident Title</th><th>Incident Type</th><th>Status</th><th>Date Created</th></tr>";
-        foreach($table as $row){
-                echo "<tr>";
-                foreach($row as $value){
-                        echo "<td>$value</td>";
-                }
-                echo "</tr>";
-        }
-
-        $db->close();
-
-
-?>
-
 <html lang="en">
 <head>
  <title> CSIRT Homepage </title>
@@ -50,15 +13,25 @@ $sql = "SELECT * FROM Incident ORDER BY dateCreated DESC";
 
 <br><br>
 
- <table class="IncidentsTable">
-  <tr>
-    <td id="number"><center>Incident #</center></td>
-    <td><center>Date Created</center></td>
-    <td id="title"><center>Title</center></td>
-    <td><center>Status</center></td>
-    <td><center>Type</center></td>
+<table class="IncidentsTable">
+  <tr class="TableHeader">
+    <th id="headerItem"><center>Incident #</center></th>
+    <th id="headerItemTitle"><center>Title</center></th>
+    <th id="headerItem"><center>Type</center></th>
+    <th id="headerItem"><center>Status</center></th>
+    <th id="headerItem"><center>Date Created</center></th>
+  </tr>
+  <tr class="TableRow">
   </tr>
 </table>
 
 </body>
 </html>
+
+<?php
+	include ("connection.php");	//connects to db
+	
+	$sql = "SELECT * FROM Incident ORDER BY dateCreated DESC";
+        $result = $db->query($sql);
+	
+?>

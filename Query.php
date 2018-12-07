bc5db814440a59c22504ef22813f86d72e836154
Lynoska Garcia <lynoskagarcia@mail.adelphi.edu>
2018-12-06 22:23:36 -0500

    Testing retrieving individual variables

--- Query.php
+++ Query.php
@@ -14,11 +14,11 @@ if(!$result){
 else{
   echo "<br>" . $result->num_rows. " ticket(s) displayed.<br>";
 
-  //two methods
   $table = $result->fetch_all();
-  //var_dump($table);
+
   echo "<table border = '1'>";
   echo "<tr><th>Incident #</th><th>Incident Title</th><th>Incident Type</th><th>Status</th><th>Date Created</th></tr>";
+
   foreach($table as $row){
     echo "<tr>";
     foreach($row as $value){

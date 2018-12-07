f50e48e197f647edf6e7fa89c6346edaac6648a8
Lynoska Garcia <lynoskagarcia@mail.adelphi.edu>
2018-12-06 20:53:08 -0500

    Testing retrieving individual variables

--- Query.php
+++ Query.php
@@ -1,33 +1,43 @@
 <?php
-        include ("connection.php");
-	echo "<link rel='stylesheet' type='text/css' href='Query.css' />";
+include ("connection.php");
+echo "<link rel='stylesheet' type='text/css' href='Query.css' />";
 
-	if (isset($_REQUEST['incidentNo']))
-        {       $incidentNo = $_REQUEST['incidentNo'];  }
+if (isset($_REQUEST['incidentNo']))
+{       $incidentNo = $_REQUEST['incidentNo'];  }
 
-	$sql = "SELECT * FROM Incident WHERE incidentNo = $incidentNo";
-	$result = $db->query($sql);
+$sql = "SELECT * FROM Incident WHERE incidentNo = $incidentNo";
+$result = $db->query($sql);
 
-	if(!$result){
-                echo "Oops! " . $db->error;
-        }
-        else{
-                echo "<br>" . $result->num_rows. " ticket(s) displayed.<br>";
+if(!$result){
+  echo "Oops! " . $db->error;
+}
+else{
+  echo "<br>" . $result->num_rows. " ticket(s) displayed.<br>";
 
-	//two methods
-        $table = $result->fetch_all();
-        //var_dump($table);
-        echo "<table border = '1'>";
-        echo "<tr><th>Incident #</th><th>Incident Title</th><th>Incident Type</th><th>Status</th><th>Date Created</th></tr>";
-        foreach($table as $row){
-                echo "<tr>";
-                foreach($row as $value){
-                        echo "<td>$value</td>";
-                }
-                echo "</tr>";
-        }
+  //two methods
+  $table = $result->fetch_all();
+  //var_dump($table);
+  echo "<table border = '1'>";
+  echo "<tr><th>Incident #</th><th>Incident Title</th><th>Incident Type</th><th>Status</th><th>Date Created</th></tr>";
+  foreach($table as $row){
+    echo "<tr>";
+    foreach($row as $value){
+      echo "<td>$value</td>";
+    }
+    echo "</tr>";
+  }
+
+  $title = $table->$title;
+  echo "Title:" . $title;
+
+  $title2 = $incidentNo->$title;
+  echo "Title 2:" . $title2;
+
+  $title3 = $title;
+  echo "Title 3:" . $title3;
+
 }
 
-        $db->close();
+$db->close();
 
 ?>

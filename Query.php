1bf80b1bc89e870a0f51dcf9511e434018ac20a0
Lynoska Garcia <lynoskagarcia@mail.adelphi.edu>
2018-12-08 14:36:53 -0500

    Added description to the query

--- Query.php
+++ Query.php
@@ -1,57 +1,61 @@
 <html lang = "en">
- <link rel="stylesheet" type ="text/css" href="Homepage.css"/>
+<link rel="stylesheet" type ="text/css" href="Homepage.css"/>
 <body>
 
-<table class="IncidentsTable">
-  <tr class="TableHeader">
-    <th id="headerItem"><center>Incident #</center></th>
-    <th id="headerItemTitle"><center>Title</center></th>
-    <th id="headerItem"><center>Type</center></th>
-    <th id="headerItem"><center>Status</center></th>
-    <th id="headerItem"><center>Date Created</center></th>
-  </tr>
+  <table class="IncidentsTable">
+    <tr class="TableHeader">
+      <th id="headerItem"><center>Incident #</center></th>
+      <th id="headerItemTitle"><center>Title</center></th>
+      <th id="headerItem"><center>Type</center></th>
+      <th id="headerItem"><center>Status</center></th>
+      <th id="headerItem"><center>Date Created</center></th>
+    </tr>
+    <tr>
+      <th id="headerItemDescription"><center>Description</center></th>
+    </tr>
 
-<?php
-        include ("connection.php");     //connects to db
-        if (isset($_REQUEST['incidentNo']))
-	{       $incidentNo = $_REQUEST['incidentNo'];  }
-	$sql = "SELECT * FROM Incident WHERE incidentNo = $incidentNo";
-	$result = $db->query($sql);
-	if (!$result){
-		echo "Oops! " . $db->error;
-	}
-	else{
 
-        if (isset($_REQUEST['incidentNo']))
-	{       $incidentNo = $_REQUEST['incidentNo'];  }
+    <?php
+    include ("connection.php");     //connects to db
+    if (isset($_REQUEST['incidentNo']))
+    {       $incidentNo = $_REQUEST['incidentNo'];  }
+    $sql = "SELECT * FROM Incident WHERE incidentNo = $incidentNo";
+    $result = $db->query($sql);
+    if (!$result){
+      echo "Oops! " . $db->error;
+    }
+    else{
 
-	$sql = "SELECT * FROM Incident WHERE incidentNo = $incidentNo";
-	$result = $db->query($sql);
+      if (isset($_REQUEST['incidentNo']))
+      {       $incidentNo = $_REQUEST['incidentNo'];  }
 
-	if (!$result){
-		echo "Oops! " . $db->error;
-	}
-	else{
-          $table = $result->fetch_all();
-          {
-                foreach($table as $row) {
-                ?>
-               		 <tr class = "TableRow">
-                <?php
-                
-                	foreach($row as $value)
-                	{
-                        	echo "<td>$value</td>";
-                	}
-                }
-                ?>
-                </tr>
-<?php
-          }
-	}
-}
-?>
-</table>
+      $sql = "SELECT * FROM Incident WHERE incidentNo = $incidentNo";
+      $result = $db->query($sql);
 
-</body>
-</html>
+      if (!$result){
+        echo "Oops! " . $db->error;
+      }
+      else{
+        $table = $result->fetch_all();
+        {
+          foreach($table as $row) {
+            ?>
+            <tr class = "TableRow">
+              <?php
+
+              foreach($row as $value)
+              {
+                echo "<td>$value</td>";
+              }
+            }
+            ?>
+          </tr>
+          <?php
+        }
+      }
+    } //
+      ?>
+    </table>
+
+  </body>
+  </html>

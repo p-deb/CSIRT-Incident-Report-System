4da7a1ffe34f65e602267492edaafb8a9daa21fa
p-deb <proggadeb@mail.adelphi.edu>
2018-12-09 02:18:13 -0500

    database updates consistently, but is one incident behind in where the data is recorded

--- NewIncident.php
+++ NewIncident.php
@@ -1,6 +1,6 @@
 <?php 
      include("connection.php");// Connects to db
-//include("home.php");
+
      # Inserts Incident title, category, and status into incident table
      # Incident No is also incremented by 1
      if (isset($_REQUEST['IncidentTitle']))
@@ -33,16 +33,19 @@ if (isset($_REQUEST['ip']))
         {       $ipRelation = $_REQUEST['ipRelation'];     }
 
 
-$sql = "INSERT INTO `Incident` (incidentNo, incidentType, dateCreated, incidentStatus, incidentTitle ) VALUES (DEFAULT, '$Category', current_timestamp(), '$Status', '$IncidentTitle');";
-$last = mysqli_query($db, "SELECT incidentNo FROM Incident ORDER BY incidentNo DESC;");
-$lastIncident = mysqli_fetch_row($last);
-echo "The most recent incident entered is: " .$lastIncident[0]; ?> <br> <?php
+$sql = "INSERT INTO `Incident` (incidentType, dateCreated, incidentStatus, incidentTitle ) VALUES ('$Category', current_timestamp(), '$Status', '$IncidentTitle');";
 $sql .= "INSERT INTO `Participant` (lastName, firstName, jobTitle, email, reasonForIncident) VALUES ('$LastName', '$FirstName', '$Job', '$Email', '$Relation');";
-?> <br> <?php
-echo "Incident number:" . $lastIncident[0];
-$sql .= "INSERT INTO `Participant_has_Incident` VALUES ('$lastIncident[0]', '$LastName', '$FirstName');";               
-$sql .= "INSERT INTO `IP Address` VALUES ('$lastIncident[0]', '$IP', '$ipRelation');";
-$sql .= "INSERT INTO `Comments` VALUES (current_timestamp(), '$textarea', '$lastIncident[0]');";
+
+$last = mysqli_query($db, "SELECT MAX(incidentNo) AS maxNo FROM incident");
+$row = mysqli_fetch_assoc($last);
+$lastIncident = $row['maxNo'];
+
+echo "The most recent incident entered is: " .$lastIncident; ?> <br><br> <?php
+echo "Incident number:" . $lastIncident;
+
+$sql .= "INSERT INTO `Participant_has_Incident` VALUES ('$lastIncident', '$LastName', '$FirstName');";               
+$sql .= "INSERT INTO `IP Address` VALUES ('$lastIncident', '$IP', '$ipRelation');";
+$sql .= "INSERT INTO `Comments` VALUES (current_timestamp(), '$textarea', '$lastIncident');";
 
 if ($db->multi_query($sql) === TRUE)
 { ?> <br>
@@ -52,4 +55,6 @@ else
 {echo "Error: " .$sql . "<br>" . $db->error;}
 
 $db->close();
+header("refresh: 3; url = Homepage.php");
 ?>
+

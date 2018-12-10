e9ac39e9e0417a0128d95b1c2ef8d030c0c51fa8
p-deb <45210221+p-deb@users.noreply.github.com>
2018-12-09 21:47:36 -0500

    Update session.php

--- session.php
+++ session.php
@@ -2,12 +2,14 @@
 	include("connection.php");	// connects to db
 	session_start();
 
-	$validate = $_SESSION['login_user'];
+	$validate = $_SESSION['login_user'];	//Sets which user is logging in
 
+	// Gets the username from the db that the user logging in entered
 	$sql = mysqli_query($db, "SELECT username FROM incidentResponders WHERE username = '$validate'");
 	$row = mysqli_fetch_array($sql, MYSQLI_ASSOC);
         $login_session = $row['username'];
 
+	// If username input incorrect, redirect back to login page
         if (!isset($_SESSION['login_user']))
         { echo "<script> location.href='index.html'; </script>"; }
 

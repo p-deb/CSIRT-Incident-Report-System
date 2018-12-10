40b6509a16bcbf34e8e4a0b44b867c19a6c34e8e
p-deb <proggadeb@mail.adelphi.edu>
2018-12-09 21:31:27 -0500

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -2,6 +2,7 @@
 	session_start();
 	if (session_destroy())
 	{
+		// End session and redirect user to login page
                 echo "<script> location.href='index.html'; </script>";
         }
 ?>
